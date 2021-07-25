62e116ea822bd4f42e9b3f4a7dbea6515dd9d7dd
nishimototo <85095171+nishimototo@users.noreply.github.com>
2021-07-25 22:32:47 +0900

    Update book_comments_controller.rb

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,9 +1,9 @@
 class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
-    @comment = current_user.book_comments.new(book_comment_params)
-    @comment.book_id = @book.id
-    if @comment.save
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    if @book_comment.save
       redirect_to book_path(@book)
     else
       @user = @book.user
@@ -14,8 +14,8 @@ class BookCommentsController < ApplicationController
 
   def destroy
     @book = Book.find(params[:book_id])
-    @comment = current_user.book_comments.find_by(book_id: @book.id)
-    @comment.destroy
+    @book_comment = current_user.book_comments.find_by(book_id: @book.id)
+    @book_comment.destroy
     redirect_to book_path(@book)
   end
 
