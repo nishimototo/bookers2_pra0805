c71ea537aec357e0fdab83db9f5c22fadd8d59b1
nishimototo <dkspm2436@gmail.com>
2021-08-05 02:04:09 +0000

    フォロー機能完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,11 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
 
   has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id, dependent: :destroy
-  has_many :following, through: :active_relationships, source: :follower
+  has_many :followings, through: :active_relationships, source: :follower
   has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :following
 
   def followed_by?(user)
-    passive_relationships.where(following_id: user.id).exists?
+    passive_relationships.find_by(following_id: user.id).present?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "homes#top"
   get "home/about" => "homes#about"
+  
   resources :users do
     resource :relationships, only: [:create, :destroy]
+      get :follows, on: :member
+      get :followers, on: :member
   end
 
   resources :books do
