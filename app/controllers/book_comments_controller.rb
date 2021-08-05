def80bbe78c5bc654b2ba52ac5258c47d203b0c9
nishimototo <dkspm2436@gmail.com>
2021-08-05 05:55:36 +0000

    コメントといいねの非同期完了

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,7 +4,8 @@ class BookCommentsController < ApplicationController
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
     if @book_comment.save
-      redirect_to book_path(@book)
+      #redirect_to book_path(@book)
+
     else
       @user = @book.user
       @newbook = Book.new
@@ -16,7 +17,7 @@ class BookCommentsController < ApplicationController
     @book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.find_by(book_id: @book.id)
     @book_comment.destroy
-    redirect_to book_path(@book)
+    #redirect_to book_path(@book)
   end
 
   private
